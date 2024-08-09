eaf94de0d33e52ad241a9093267bb6e281b62525
Faraz Ibrar <farazibrar9697@gmail.com>
2024-08-09 16:54:07 +0500

    Adding Family Tracker with new concept of one to meny relationship

--- Secrets Project/index.js
+++ Secrets Project/index.js
@@ -12,7 +12,7 @@ const port = 3000;
 app.use(express.static("public"));
 
 // 4. When the user goes to the home page it should render the index.ejs file.
-
+app.rende
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
 // secret and the username of the secret.
 
